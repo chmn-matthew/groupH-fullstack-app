ccc1f4d198d242e207076c6ce381aee054985acc
Matthew Neil B. Cabrillos <138511859+chmn-matthew@users.noreply.github.com>
2025-05-19 12:45:38 +0800

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://user-management-system-angular-master-production.up.railway.app'
+    apiUrl: 'https://grouph-fullstack-app-production.up.railway.app'
 }
 
 // CHANGE
